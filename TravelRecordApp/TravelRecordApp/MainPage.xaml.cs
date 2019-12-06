f11cd648622d851e8708a699c83238a4e52c206d
Javier Gonzalez <javier.glez.martin@gmail.com>
2019-12-07 00:40:55 +0100

    Define, implement and use an asnc method

--- TravelRecordApp/TravelRecordApp/MainPage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
@@ -28,6 +29,20 @@ namespace TravelRecordApp
                 
             await Navigation.PushAsync(new HomePage());
 
+        }
+
+        private async void cancelSubscriptionButton_Clicked(object sender, EventArgs e)
+        {
+            // Calling an async method (use async and await the result defining it as var)
+            var result = await cancelSubscription();
+            resultCancelSusbscriptionLabel.Text = result;
+        }
+
+        // Definition and implementation of an async method (use async Task<returnTpye>)
+        private async Task<string> cancelSubscription()
+        {
+            Thread.Sleep(2000);
+            return "Cancelation succeded";
         }
     }
 }
