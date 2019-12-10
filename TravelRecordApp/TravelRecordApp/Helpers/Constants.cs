915c96e55c9cfd7ee9a424e4015c0498cea87626
Javier Gonzalez <javier.glez.martin@gmail.com>
2019-12-10 22:41:27 +0100

    Create URL for http REST Request

---
+++ TravelRecordApp/TravelRecordApp/Helpers/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TravelRecordApp.Helpers
+{
+    public class Constants
+    {
+        public const string VENUE_SEARCH = "https://api.foursquare.com/v2/venues/search?ll={0},{1}&client_id={2}&client_secret={3}&v={4}";
+        public const string CLIENT_ID = "HOXKV5QTNLC0UY2Y2ITSDKGRLESDWV5N2B2GHHTDDHZYPF5N";
+        public const string CLIENT_SECTRET = "UNAUK3TLZR0J0XE2MZ3YNLIU1M4BBNPQXDKSAPGN1IF1CV2P";
+    }
+}
---
+++ TravelRecordApp/TravelRecordApp/Model/Venue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TravelRecordApp.Helpers;
+
+namespace TravelRecordApp.Model
+{
+    public class Venue
+    {
+        public static string GenerateURL(double latitue, double longitude)
+        {
+            return string.Format(Constants.VENUE_SEARCH, latitue, longitude, Constants.CLIENT_ID, Constants.CLIENT_SECTRET, DateTime.Now.ToString("yyyyMMdd"));
+        }
+    }
+}
